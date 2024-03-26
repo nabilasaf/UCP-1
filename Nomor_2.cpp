8081f6d415219a6f75f2c8e5220585cf414d1377
nabilasaf <nabilasafitri861@gmail.com>
2024-03-26 16:28:08 +0700

    Nomor_2

---
+++ Nomor_2.cpp
@@ -0,0 +1,11 @@
+void SelectionSort(){
+    do{
+        for (int j = 0; j <= n - 2; j++) // step 1 memasukkan  j dari 0 ke n - 2
+        int min_index = j; // step 2a untuk menentukan bahwa minimal index = j
+            if (arr[i] < arr[j]) { // step 2c mengulang pengerjaan dari j + 1 ke n - 1
+            int arr[j] = arr[i] // step 3 menukarkan arr[j] dengan arr[min_index]
+        }
+        
+        }
+    }
+}
