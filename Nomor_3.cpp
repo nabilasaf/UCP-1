ee0157d4893477ca0838daeb8720b0a41beb7ecd
nabilasaf <nabilasafitri861@gmail.com>
2024-03-26 16:28:19 +0700

    Nomor_3

---
+++ Nomor_3.cpp
@@ -0,0 +1,8 @@
+int main()
+{
+    input();  //memanggil prosedur untuk menginput data
+    bubbleSortArray();  //memanggil prosedur bubblesort untuk proses mengurutkan data
+    display();//memanggil prosedur untuk menampilkan data yang telah urut
+    system("pause"); //memerintah system untuk memberhentikan ketika sudah sesuai
+    return 0;//jika sudah sesuai akan kembali ke 0
+}
